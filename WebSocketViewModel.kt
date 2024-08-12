721df6a03470301454d07a00554b7864842ef725
Thirumalaibarath <162014344+Thirumalaibarath@users.noreply.github.com>
2024-08-12 20:32:14 +0530

    Update WebSocketViewModel.kt

--- WebSocketViewModel.kt
+++ WebSocketViewModel.kt
@@ -1,37 +1,29 @@
 package com.example.hackathon
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import io.socket.client.IO
 import io.socket.client.Socket
-import io.socket.emitter.Emitter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.WebSocket
-import okhttp3.WebSocketListener
-import okio.ByteString
 import org.json.JSONArray
 import java.net.URISyntaxException
 
-class WebSocketViewModel:ViewModel() {
+class WebSocketViewModel {
 
     private lateinit var mSocket: Socket
 
     private val initial_grid = GameRequest(
-        requestGameState = listOf(
-            listOf("#FF5733", "#FF5733", "#FF5733"),
-            listOf("#FF5733", "#FF5733", "#FF5733"),
-            listOf("#FF5733", "#FF5733", "#FF5733")
-        )
+        requestGameState = List(12) {
+            List(8) { "#FFFFFF" }
+        }
     )
 
-    var gameState: GameRequest = initial_grid
+    var gameState by mutableStateOf(
+        GameRequest(
+            requestGameState = initial_grid.requestGameState
+        )
+    )
 
     init {
         try {
@@ -46,28 +38,24 @@ class WebSocketViewModel:ViewModel() {
         mSocket.on(Socket.EVENT_CONNECT) {
             Log.d("Socket.IO", "Connected to server")
             mSocket.emit("message", "Hello from Android!")
-
-            val intArray = arrayOf(1, 2, 3, 4, 5)
-
-            val jsonArray = JSONArray(intArray)
-
-            mSocket.emit("message", jsonArray)
         }
 
         mSocket.on("counter") { args ->
+            val dataclass: MutableList<MutableList<String>> = MutableList(12) {
+                MutableList(8) { "" }
+            }
+
             val jsonArray = args[0] as JSONArray
 
             for (i in 0 until jsonArray.length()) {
                 val innerJsonArray = jsonArray.getJSONArray(i)
-                val innerList = mutableListOf<String>()
                 for (j in 0 until innerJsonArray.length()) {
-                    innerList.add(innerJsonArray.getString(j))
+                    dataclass[i][j] = innerJsonArray.getString(j)
                 }
-                gameState.requestGameState.
             }
 
-            gameState = args[0] as GameRequest
-            Log.d("Socket.IO", args[0].toString())
+            gameState = GameRequest(requestGameState = dataclass)
+            println(gameState.requestGameState)
         }
 
         mSocket.on(Socket.EVENT_DISCONNECT) {
@@ -83,18 +71,12 @@ class WebSocketViewModel:ViewModel() {
     }
 
 
-    fun sendMessage() {
-
-        val intArray = arrayOf(1, 2, 3, 4, 5)
-        val jsonArray = JSONArray(intArray)
-
-        mSocket.emit("message", jsonArray)
+    fun sendMessage(rowInt: Int,columnInt: Int,color : String) {
+        mSocket.emit("rowInt",rowInt)
+        mSocket.emit("columnInt",columnInt)
+        mSocket.emit("color",color)
 
     }
 
-
-    fun disconnectSocket() {
-        mSocket.disconnect()
-    }
 }
 
