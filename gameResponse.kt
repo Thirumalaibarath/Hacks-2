96485dbd6511be4f465f4b0c4cbad93bc1469706
Thirumalaibarath <162014344+Thirumalaibarath@users.noreply.github.com>
2024-08-12 20:31:38 +0530

    Update gameResponse.kt

--- gameResponse.kt
+++ gameResponse.kt
@@ -1,12 +1,5 @@
 package com.example.hackathon
 
-import androidx.compose.ui.graphics.Color
-
-
-data class GameResponse(
-    val responseGameState : List<List<String>>,
-)
-
 data class GameRequest(
-    val requestGameState : List<List<String>>,
+    var requestGameState : List<List<String>>,
 )
